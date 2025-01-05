e08253a3bf61e6a62f964b03b8f9852432456edb
PerikiyoXD <perikiyoxd@gmail.com>
2025-01-05 01:49:34 +0100

    Use Append instead of operator +=

--- src/Browser.cpp
+++ src/Browser.cpp
@@ -148,7 +148,7 @@ void Browser::LaunchGame(const String& host, uint16_t port)
     data.game.arguments.Append("nickname", m_settings.nickname);
 
     if (m_settings.windowed)
-        data.game.arguments += "-windowed";
+        data.game.arguments.Append("-windowed");
 
     Path curPath = std::filesystem::current_path();
 
